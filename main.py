03013c9abdad18de3b2224e617ba9bf03a8952d5
João Américo Caiafa <joaocaiafa@outlook.com>
2020-05-14 09:20:06 -0300

    Remove Key

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import time
 import requests
 
-headers = {"Authorization": "Bearer YOUR KEY HERE "}
+headers = {"Authorization": "Bearer f8d9632f6225a190ac5d58756a0c25f72b078937"}
 
 
 def run_query(json, headers):  # Função que executa uma request pela api graphql
@@ -77,7 +77,7 @@ nodes = result["data"]["search"]["nodes"]  # separar a string para exibir apenas
 next_page = result["data"]["search"]["pageInfo"]["hasNextPage"]
 
 page = 0
-while next_page and total_pages < 50:
+while next_page and total_pages < 25:
     total_pages += 1
     cursor = result["data"]["search"]["pageInfo"]["endCursor"]
     next_query = query.replace("{AFTER}", ", after: \"%s\"" % cursor)
@@ -95,57 +95,9 @@ base = csv.writer(file)
 print("Salvando Repositorios:\n")
 isTheLastName = False
 for node in nodes:
-    if node['name'] == lastName:
-        isTheLastName = True
-    if isTheLastName or lastName == "":
-        print("Buscando issues de " + node['name'])
-        query = """
-                    query example{
-                        repository(name:{NAME}, owner:{OWNER}){
-                            issues(first:100{AFTER}, orderBy:{field:CREATED_AT, direction:DESC}){
-                                pageInfo{
-                                    endCursor
-                                    hasNextPage
-                                }
-                                nodes{
-                                title
-                                number
-                                }
-                            }
-                        }
-                    }       
-                    """
-        query = query.replace("{NAME}", '"' + node['name'] + '"')
-        query = query.replace("{OWNER}", '"' + node['owner']['login'] + '"')
-        finalQuery = query.replace("{AFTER}", "")
-
-        json = {
-            "query": finalQuery, "variables": {}
-        }
-
-        print("Encontrado " + str(node['issues']['totalCount']) + " Issues \n[", end='')
-        result = run_query(json, headers)  # Executar a Query
-        nodes = result["data"]["repository"]["issues"]['nodes']
-        next_page = result["data"]["repository"]["issues"]["pageInfo"]["hasNextPage"]
-
-        num = 0
-        while next_page:
-            num += 1
-            cursor = result["data"]["repository"]["issues"]["pageInfo"]["endCursor"]
-            next_query = query.replace("{AFTER}", ", after: \"%s\"" % cursor)
-            json["query"] = next_query
-            result = run_query(json, headers)
-            nodes += result["data"]["repository"]["issues"]['nodes']
-            next_page = result["data"]["repository"]["issues"]["pageInfo"]["hasNextPage"]
-            if num % 10 == 0:
-                print(".", end='')
-        print("]")
-
-        print("Salvando Issues")
-        for issues in nodes:
-            base.writerow((node['owner']['login'], node['name'], str(node['stargazers']['totalCount']),
-                           str(node['issues']['totalCount']), str(node['closedIssues']['totalCount']),
-                           str(node['openIssues']['totalCount']), issues['title'], str(issues['number'])))
+    base.writerow((node['owner']['login'], node['name'], str(node['stargazers']['totalCount']),
+                   str(node['issues']['totalCount']), str(node['closedIssues']['totalCount']),
+                   str(node['openIssues']['totalCount'])))
 print("]\nProcesso concluido")
 file.close()
 print("\n ------------- Fim da execução ------------- \n")
